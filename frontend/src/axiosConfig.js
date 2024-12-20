33d9007e65f5829215c8c00b68e3354a28d4a8a1
sazvan.saeed <sazvan.saeed@hotmail.de>
2024-12-20 03:12:27 +0100

    change post URL to env variables

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/run', // Adjust the baseURL to your server endpoint
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/run', // Adjust the baseURL to your server endpoint
   headers: {
     'Content-Type': 'application/json'
   },
